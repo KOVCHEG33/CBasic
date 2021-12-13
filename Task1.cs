eaee56deb150ab318935676a5dd48fb7751eff88
KOVCHEG33 <96081656+KOVCHEG33@users.noreply.github.com>
2021-12-13 21:56:38 +0200

    Lesson 2
    В этот раз сдаю через ГИТ =)

---
+++ Task1.cs
@@ -0,0 +1,24 @@
+﻿//(Arthur Solomon) 1. Написать метод, возвращающий минимальное из трёх чисел. 
+
+Console.WriteLine("Enter the first number.");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number.");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third number.");
+int c = int.Parse(Console.ReadLine());
+
+if (a < b && a < c)
+{
+    Console.WriteLine($"The smallest number is {a}!");
+    Console.ReadLine();
+}
+else if (b < a && b < c)
+{
+    Console.WriteLine($"The smallest number is {b}!");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine($"The smallest number is {c}!");
+    Console.ReadLine();
+}
---
+++ Task2.cs
@@ -0,0 +1,13 @@
+﻿//(Arthur Solomon) 2. Написать метод подсчета количества цифр числа.
+Console.WriteLine("Enter the number.");
+int firstN = int.Parse(Console.ReadLine());
+int N = firstN / 10;
+int digits = 1;
+
+while (N != 0) 
+{
+    digits = digits + 1;
+    N = N / 10;
+}
+Console.WriteLine($"Number {firstN} consists of {digits} digits!");
+Console.ReadLine();
